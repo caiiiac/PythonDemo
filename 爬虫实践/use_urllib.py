9d6efc879c0a4c87a4343e0a6d22320cc28d3858
caiiiac <caiiiac@163.com>
2017-05-24 12:00:09 +0800

    添加HTTP请求头

--- 爬虫实践/use_urllib.py
+++ 爬虫实践/use_urllib.py
@@ -3,6 +3,15 @@
 
 import urllib.request
 
+'添加HTTP请求头'
+
+req = urllib.request.Request('http://daily.zhihu.com/')
+req.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36')
+f = urllib.request.urlopen(req)
+print(f.read().decode('utf-8'))
+
+
+
 #获取baidu.com主页，并显示其前300个字节。
 # with urllib.request.urlopen('http://www.baidu.com') as f:
 # 	print('解码前:', f.read(300))
@@ -12,6 +21,7 @@ import urllib.request
 # with urllib.request.urlopen('http://www.baidu.com') as f:	
 # 	print('解码后:', f.read(50).decode('utf-8'))
 
+
 #也可以直接赋值使用
-f = urllib.request.urlopen('http://daily.zhihu.com/')
-print('第二种写法:',f.read().decode('utf-8'))
+# f = urllib.request.urlopen('http://daily.zhihu.com/')
+# print('第二种写法:',f.read().decode('utf-8'))
